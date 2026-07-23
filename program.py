3cd768a5a6bd2be2c4447b1ce44920b5768c7c8c
abhi2deo <abhinavdeo72@gmail.com>
2026-07-23 09:10:29 +0530

    removed inputs

--- program.py
+++ program.py
@@ -1,16 +1,4 @@
-print("1.Withdraw\n2.Deposit\n3.Check balance")
 balance = 10000000000
-
-ch = int(input("Choose operation: "))
-if(ch==1):
-    amt = int(input("Enter amount: "))
-    balance -= amt
-    print("Withdrawal successful")
-elif(ch==2):
-    amt = int(input("Enter amount: "))
-    balance += amt
-    print("Deposit successful")
-elif(ch==3):
-    print("Balance: ",balance)
-else:
-    print("Invalid input!")
+amt = int(input("Enter amount: "))
+balance -= amt
+print("Withdrawal successful: "+balance)
